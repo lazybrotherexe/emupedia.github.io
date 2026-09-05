e70a0d7363462aa7d6ef73f4e5615ec4589c6c7c
Lazy Brother <selam@lazybrother.com.tr>
2026-09-05 05:29:07 +0300

    Create [[path]].js

---
+++ functions/emupedia-game-wolfenstein-3d-canvas/[[path]].js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+  const path = context.params.path || "";
+  const sourceUrl =
+    "https://emupedia.net/emupedia-game-wolfenstein-3d-canvas/" + path;
+
+  const originalUrl = new URL(context.request.url);
+  const targetUrl = new URL(sourceUrl);
+  targetUrl.search = originalUrl.search;
+
+  const request = new Request(targetUrl.toString(), context.request);
+
+  return fetch(request);
+}
